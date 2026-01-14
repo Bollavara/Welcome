1f41884159da3e38f32dee264193a97f66ba0963
Harika <bharika903@gmail.com>
2026-01-14 10:37:44 +0530

    TC_Functions

--- TC_Functions.py
+++ TC_Functions.py
@@ -14,7 +14,20 @@ def sub(a,b):
     return a-b,a
 print(sub(100,20))
 
+def hello(greeting="Hello",name="world"):
+    print('%s,%s'%(greeting,name))
+hello() #default parameters will be used
+hello('Greetings','Harika')
 
+#Pass n number of parameters
+def print_param(*params):
+    print(params)
+print_param('Testing')
+print_param(1,2,3,4)
 
+#Named parameters passing
+def print_param1(**params):
+    print(params)
+print_param1(x=1,y=2,z=3)
 
 
