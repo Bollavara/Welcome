d97167265e3da67069a946cd88f2c40414480715
Harika <bharika903@gmail.com>
2026-01-14 09:52:59 +0530

    TC_Functions

---
+++ TC_Functions.py
@@ -0,0 +1,20 @@
+#basic syntax of python functions
+def add(a,b):  #Definefirst
+    print(a+b)
+
+#Return Value - print
+def sub(a,b):
+    return a-b
+
+add(10,20)
+print(sub(100,20))
+
+#can Return multiple values - print
+def sub(a,b):
+    return a-b,a
+print(sub(100,20))
+
+
+
+
+
